0c0e0b03efad0f1ca6a67c722f030fc8f7769cc9
benzoic <1738831615@qq.com>
2026-06-28 21:35:49 +0800

    fix: fix local-environment develop connecting server problem

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://38.22.95.108:8080',
+        target: 'http://38.22.95.108:8080',
         changeOrigin: true,
         ws: true
       }
