2c800b57987a15171d9afa40f2261868293d1920
benzoic <1738831615@qq.com>
2026-06-28 14:32:13 +0800

    change ip

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://38.22.95.108:8080',
+        target: 'http://38.22.95.108:808',
         changeOrigin: true,
         ws: true
       }
